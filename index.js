1bff1d03fe64b40903e25497abdbc03df9c007c8
Aviya Amasay <aviya.amasay@allcloud.io>
2022-02-28 17:21:45 +0200

    ftp

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ class App{
 
     parseCSV(csv){
         console.log({csv});
-        let lines = csv.split('\n');
+        let lines = csv.replace(/\s|\r/g, '').split('\n');
         let result = [];
 
         let headers = lines[0].split(',');
